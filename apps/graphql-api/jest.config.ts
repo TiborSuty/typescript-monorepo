8a0dae2e9b3f16e820be2921d68f91a392f303f4
Tibor Suty <tiborsuty231@gmail.com>
2023-09-23 23:55:58 +0200

    Initial nestjs

--- apps/graphql-api/jest.config.ts
+++ apps/graphql-api/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'qraphql-api',
+  displayName: 'graphql-api',
   preset: '../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/qraphql-api',
+  coverageDirectory: '../coverage/graphql-api',
 };
